7de686d352a9ce2a57974859803516403867f59c
ChanyoungKim <davidkjcy@gmail.com>
2022-08-12 19:56:49 +0900

    First Addon

--- Queue Using Class/Queue.cpp
+++ Queue Using Class/Queue.cpp
@@ -1,1 +1,45 @@
 #include "Queue.h"
+#include <iostream>
+
+void Queue::PrintInfo()
+{
+	std::cout << "<STACK>" << std::endl;
+	std::cout << "[1] enqueue" << std::endl;
+	std::cout << "[2] dequeue" << std::endl;
+	std::cout << "[3] exit" << std::endl;
+	std::cout << "---------------" << std::endl;
+}
+
+void Queue::PrintQueue()
+{
+	if (mTailIndex < 0)
+	{
+		std::cout << "EMPTY!!" << std::endl;
+		return;
+	}
+
+	for (int i = mHeadIndex; i <= mTailIndex; i++)
+	{
+		std::cout << mQueue[i] << std::endl;
+	}
+}
+
+void Queue::Enqueue(int value)
+{
+	if (mTailIndex >= STACK_SIZE - 1)
+	{
+		std::cout << "QUEUE IS FULL!" << std::endl;
+		return;
+	}
+
+}
+
+void Queue::Dequeue()
+{
+
+}
+
+void Queue::ProcessUI()
+{
+
+}
--- Queue Using Class/Queue.h
+++ Queue Using Class/Queue.h
@@ -1,6 +1,25 @@
 #pragma once
 
+const int STACK_SIZE{ 10 };
+
+enum Command
+{
+	ENQUEUE = 1,
+	DEQUEUE = 2,
+	EXIT = 3
+};
+
 class Queue
 {
+	int mQueue[STACK_SIZE]{};
+	int mHeadIndex{ -1 };
+	int mTailIndex{ -1 };
+
+public:
+	void PrintInfo();
+	void PrintQueue();
+	void Enqueue(int value);
+	void Dequeue();
+	void ProcessUI();
 };
 
